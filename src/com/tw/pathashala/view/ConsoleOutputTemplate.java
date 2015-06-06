92ce6c4d2fbb912dc2466c4234e04fdebfebd4a8
Hari krishna <harikrishnakanchi@gmail.com>
2015-06-06 17:18:43 +0530

    Added spec for ConsoleOutputTemplate wheather it is including body message or not.

--- src/com/tw/pathashala/view/ConsoleOutputTemplate.java
+++ src/com/tw/pathashala/view/ConsoleOutputTemplate.java
@@ -1,7 +1,10 @@
 package com.tw.pathashala.view;
 
 public class ConsoleOutputTemplate {
-    private String template = "";
+    private String template = "%s%s%s";
+    private Integer width = 100;
+    private String header = String.format("%s| %-97s|\n%s", divider(width), "Biblioteca", divider(width));
+    private String body = "";
 
     public String divider(Integer length) {
         String output = "";
@@ -9,5 +12,13 @@ public class ConsoleOutputTemplate {
             output = output.concat("-");
         }
         return output + "\n";
+    }
+
+    public void addToBody(String bodyItem) {
+        body = body.concat("\n" + bodyItem);
+    }
+
+    public void renderOutput() {
+        System.out.print(String.format(template, header, body, divider(width)));
     }
 }
--- tests/com/tw/pathashala/view/ConsoleOutputTemplateTest.java
+++ tests/com/tw/pathashala/view/ConsoleOutputTemplateTest.java
@@ -1,11 +1,23 @@
 package com.tw.pathashala.view;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class ConsoleOutputTemplateTest {
+
+    private final ByteArrayOutputStream outputContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setup() {
+        System.setOut(new PrintStream(outputContent));
+    }
 
     @Test
     public void testForDivider() {
@@ -15,6 +27,22 @@ public class ConsoleOutputTemplateTest {
                 "----------------\n";
 
         assertThat(divider, is(expectdDivider));
+    }
+
+    @Test
+    public void testForTemplateToIncludeBodyMessageOrNot() {
+        ConsoleOutputTemplate template = new ConsoleOutputTemplate();
+
+        template.addToBody("Welcome");
+        template.renderOutput();
+        String actual = outputContent.toString();
+
+        assertTrue(actual.contains("Welcome"));
+    }
+
+    @After
+    public void cleanUp() {
+        System.setOut(null);
     }
 
 }
