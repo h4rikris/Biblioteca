ad5656ec777389b0c3584528a7aee5d431914d99
Hari krishna <harikrishnakanchi@gmail.com>
2015-06-14 23:27:51 +0530

    Added spec to display Success Message on Valid Entry of Book name.

--- tests/com/tw/pathashala/menu/CheckOutTest.java
+++ tests/com/tw/pathashala/menu/CheckOutTest.java
@@ -1,6 +1,6 @@
 package com.tw.pathashala.menu;
 
-import com.tw.pathashala.constants.Constants;
+import com.tw.pathashala.models.Book;
 import com.tw.pathashala.models.Library;
 import com.tw.pathashala.view.ConsoleInput;
 import com.tw.pathashala.view.ConsoleOutputTemplate;
@@ -9,10 +9,8 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static com.tw.pathashala.constants.Constants.CHECKOUT_INVALID_MESSAGE;
-import static com.tw.pathashala.constants.Constants.CHECKOUT_MESSAGE;
-import static com.tw.pathashala.constants.Constants.CONTINUE;
-import static org.junit.Assert.*;
+import static com.tw.pathashala.constants.Constants.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
 
@@ -28,6 +26,8 @@ public class CheckOutTest {
     @Mock
     ConsoleInput consoleInput;
 
+    @Mock
+    Book book;
     @Test
     public void shouldReturnContinueOnExecuteMethod() {
         CheckOut checkOut = new CheckOut(outputTemplate, consoleInput, library);
@@ -76,9 +76,20 @@ public class CheckOutTest {
     @Test
     public void shouldDisplayErrorMessageOnInvalidBookEntry() {
         CheckOut checkOut = new CheckOut(outputTemplate, consoleInput, library);
-        when(library.checkOut("some")).thenReturn(false);
+        when(library.checkOut(anyString())).thenReturn(false);
+
         checkOut.execute();
 
-        verify(outputTemplate).addToBody(CHECKOUT_INVALID_MESSAGE);
+        verify(outputTemplate, times(1)).addToBody(CHECKOUT_INVALID_MESSAGE);
+    }
+
+    @Test
+    public void shouldDisplaySuccessMessageValidBookEntry() {
+        CheckOut checkOut = new CheckOut(outputTemplate, consoleInput, library);
+        when(library.checkOut(anyString())).thenReturn(true);
+
+        checkOut.execute();
+
+        verify(outputTemplate, times(1)).addToBody(CHECKOUT_SUCCESS_MESSAGE);
     }
 }
