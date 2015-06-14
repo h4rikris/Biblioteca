922a9d0d06cbaa21fb7ea2c09ed15fb622cc3547
Hari krishna <harikrishnakanchi@gmail.com>
2015-06-15 01:29:39 +0530

    Should prompt user for Book name and able to Return the corresponding book

--- tests/com/tw/pathashala/menu/ReturnBookTest.java
+++ tests/com/tw/pathashala/menu/ReturnBookTest.java
@@ -8,9 +8,12 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static com.tw.pathashala.constants.Constants.CONTINUE;
+import static com.tw.pathashala.constants.Constants.*;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ReturnBookTest {
@@ -40,4 +43,32 @@ public class ReturnBookTest {
 
         verify(library).checkedOutBooks();
     }
+
+    @Test
+    public void shouldAskForBookName() {
+        ReturnBook returnBook = new ReturnBook(outputTemplate, consoleInput, library);
+
+        returnBook.execute();
+
+        verify(consoleInput).getUserInput();
+    }
+
+    @Test
+    public void shouldAddCheckOutFooterMessageToPrompt() {
+        ReturnBook returnBook = new ReturnBook(outputTemplate, consoleInput, library);
+
+        returnBook.execute();
+
+        verify(outputTemplate).addToFooter(RETURN_MESSAGE);
+    }
+
+    @Test
+    public void shouldAbleToCheckOutTheBook() {
+        ReturnBook returnBook = new ReturnBook(outputTemplate, consoleInput, library);
+
+        returnBook.execute();
+
+        verify(library).checkOut(anyString());
+    }
+    
 }
